f436553816e5df986a780719014e82b3541d0b22
omarseddati16 <omarseddati16@gmail.com>
2025-09-22 15:36:47 +0200

    add reviews

--- routers/routers.js
+++ routers/routers.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
 const router = express.Router();
 
 const controller = require('../controller/controller')
+const reviewsController = require('../controller/reviewsController')
 
 router.get('/', controller.index);
 
 router.get('/:id', controller.show);
+
+router.get('/:id/reviews', reviewsController.showReviews);
 
 module.exports = router;
